bfa175cf411f2866513d660f4366163fb8ca34b4
Pineda93 <djjimene93@gmail.com>
2024-05-16 22:59:50 -0600

    Ejercicio 15

---
+++ Ejercicio1-15deMayo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int arreglo[9];
+for (int a = 9; a >= 0 ; a--) {
+cout << "Ingrese un numero en la posicion" << a << endl;
+cin >> arreglo[a];
+}
+return 0;
+}
